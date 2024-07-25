ec87b6b71f41077b4fa4d86679ff5e10733b5f7d
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-25 19:17:06 -0400

    Adding public_tests file for testing purposes

--- Building_Convolutional_Neural_Network.py
+++ Building_Convolutional_Neural_Network.py
@@ -2,3 +2,9 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 #from public_tests import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+np.random.seed(1)
+
