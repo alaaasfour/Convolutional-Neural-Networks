7202ceb7ceeebd039d0f4daa16ab5ff810ebdf15
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-25 19:22:28 -0400

    Adding outputs.py file

--- Building_Convolutional_Neural_Network.py
+++ Building_Convolutional_Neural_Network.py
@@ -1,10 +1,29 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-#from public_tests import *
+from public_tests import *
 
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 np.random.seed(1)
 
+
+def zero_pad(X, pad):
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values = (0, 0))
+    return X_pad
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 3)
+print ("x.shape =\n", x.shape)
+print ("x_pad.shape =\n", x_pad.shape)
+print ("x[1,1] =\n", x[1, 1])
+print ("x_pad[1,1] =\n", x_pad[1, 1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0, :, :, 0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0, :, :, 0])
+zero_pad_test(zero_pad)
