be127b883b16e538cd4b1ed967fbc150d7ef00ed
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-26 19:31:02 -0400

    Ex 1: Loading and Splitting the data into training and test sets

--- Convolutional_Neural_Networks_Application.py
+++ Convolutional_Neural_Networks_Application.py
@@ -36,9 +36,12 @@ X_test = X_test_orig / 255
 Y_train = Y_train_orig.T
 Y_test = Y_test_orig.T
 
+print("Exercise 1: Loading and Splitting the Data")
+print("==========")
 print ("number of training examples = " + str(X_train.shape[0]))
 print ("number of test examples = " + str(X_test.shape[0]))
 print ("X_train shape: " + str(X_train.shape))
 print ("Y_train shape: " + str(Y_train.shape))
 print ("X_test shape: " + str(X_test.shape))
 print ("Y_test shape: " + str(Y_test.shape))
+print("========================================")
