3788432c96e441d356094809aae2645dc9d00ee2
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-25 20:19:09 -0400

    Ex 1: Zero-Padding Function

--- Building_Convolutional_Neural_Network.py
+++ Building_Convolutional_Neural_Network.py
@@ -1,14 +1,33 @@
+"""
+In this script, we will implement a Convolutional Neural Network (CONV) and pooling (POOL) layers inn numpy,
+including both forward and backward propagation.
+"""
+
+# Packages
+# We will import all the important packages that we need.
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 from public_tests import *
-
+np.random.seed(1)
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
-np.random.seed(1)
 
 
+"""
+We will implement a Convolutional Neural Network (CONV) step by step. 
+First we will implement two helper functions: one for zero padding and the other for computing the convolution function itself.
+
+Exercise 1: Zero-Padding
+Zero-Padding adds zeros around the border of the image.
+
+The main benefits of padding are:
+    - It allows us to use a CONV layer without necessarily shrinking the height and width of the volume.
+    - It helps us keep more of the information at the border of an image. Without padding, very few values at the next layer would be affected by pixels at the edges of an image.
+    
+We will implement the following function, which pads all the images of a batch of examples X with zeros.
+"""
 def zero_pad(X, pad):
     X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values = (0, 0))
     return X_pad
@@ -27,3 +46,25 @@ axarr[0].imshow(x[0, :, :, 0])
 axarr[1].set_title('x_pad')
 axarr[1].imshow(x_pad[0, :, :, 0])
 zero_pad_test(zero_pad)
+
+
+
+def conv_single_step(a_slice_prev, W, b):
+    s = a_slice_prev * W
+    Z = np.sum(s)
+    Z = Z + float(b)
+
+    return Z
+
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+conv_single_step_test(conv_single_step)
+
+assert (type(Z) == np.float64), "You must cast the output to numpy float 64"
+assert np.isclose(Z, -6.999089450680221), "Wrong value"
+
