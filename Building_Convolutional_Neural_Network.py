46c5fd2b325a9b2a2fccea4183f34d349b5cda8e
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-25 20:20:58 -0400

    Zero-Padding function comments

--- Building_Convolutional_Neural_Network.py
+++ Building_Convolutional_Neural_Network.py
@@ -27,6 +27,14 @@ The main benefits of padding are:
     - It helps us keep more of the information at the border of an image. Without padding, very few values at the next layer would be affected by pixels at the edges of an image.
     
 We will implement the following function, which pads all the images of a batch of examples X with zeros.
+Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image.
+    
+Argument:
+    X: python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad: integer, amount of padding around each image on vertical and horizontal dimensions
+    
+Returns:
+    X_pad: padded image of shape (m, n_H + 2 * pad, n_W + 2 * pad, n_C)
 """
 def zero_pad(X, pad):
     X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values = (0, 0))
