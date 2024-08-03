77baf3bb9a042657445c2d20c0eef0d14734f36e
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-03 14:53:28 -0400

    Preprocess the Data

--- Image_Segmentation_U-Net.py
+++ Image_Segmentation_U-Net.py
@@ -1,17 +1,86 @@
+"""
+In this script, we'll be building our own U-Net, a type of CNN designed for quick, precise image segmentation, and using
+it to predict a label for every single pixel in an image - in this case, an image from a self-driving car dataset.
+
+In this script, we will do the following:
+    - Build your own U-Net
+    - Explain the difference between a regular CNN and a U-net
+    - Implement semantic image segmentation on the CARLA self-driving car dataset
+    - Apply sparse categorical crossentropy for pixelwise prediction
+"""
+# Packages
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import imageio
+import imageio.v2 as imageio
 import os
 
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import concatenate
 
-from tensorflow.keras import Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, concatenate
 from test_utils import summary, comparator
 
+# Load and Split the Data
 path = ''
 image_path = os.path.join(path, './data/CameraRGB/')
 mask_path = os.path.join(path, './data/CameraMask/')
 image_list_orig = os.listdir(image_path)
 image_list = [image_path+i for i in image_list_orig]
 mask_list = [mask_path+i for i in image_list_orig]
+
+# Check out some of the unmasked and masked images from the dataset
+N = 2
+img = imageio.imread(image_list[N])
+mask = imageio.imread(mask_list[N])
+#mask = np.array([max(mask[i, j]) for i in range(mask.shape[0]) for j in range(mask.shape[1])]).reshape(img.shape[0], img.shape[1])
+
+fig, arr = plt.subplots(1, 2, figsize=(14, 10))
+arr[0].imshow(img)
+arr[0].set_title('Image')
+arr[1].imshow(mask[:, :, 0])
+arr[1].set_title('Segmentation')
+plt.show()
+
+# Split the dataset into unmasked and masked images
+image_list_ds = tf.data.Dataset.list_files(image_list, shuffle=False)
+mask_list_ds = tf.data.Dataset.list_files(mask_list, shuffle=False)
+
+for path in zip(image_list_ds.take(3), mask_list_ds.take(3)):
+    print(path)
+
+image_filenames = tf.constant(image_list)
+masks_filenames = tf.constant(mask_list)
+
+dataset = tf.data.Dataset.from_tensor_slices((image_filenames, masks_filenames))
+
+for image, mask in dataset.take(1):
+    print(image)
+    print(mask)
+
+# Preprocess the Data
+def process_path(imege_path, mask_path):
+    image = tf.io.read_file(imege_path)
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+
+    return image, mask
+
+def preprocess(path, mask):
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    return input_image, input_mask
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
+
+
